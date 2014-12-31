334b3e9337d370dbc9a26164d863ea4b0f091033
Rob Lewis <rob@kohder.com>
2014-12-31 12:31:25 -0500

    changed admin route namespace to be a scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 if Rails.application.config.activeadmin_associations.autocomplete_models
   models = Rails.application.config.activeadmin_associations.autocomplete_models.join('|')
   Rails.application.routes.draw do
-    namespace :admin do
+    scope :admin do
       get '/autocomplete/:model' => 'autocomplete#index', :model => /(#{models})/,
         :defaults => { :format => 'json' }
     end
