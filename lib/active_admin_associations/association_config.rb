4b61c0ec12450dbd50d14df53eae84b16419aa0c
Ciprian Stratulat <ciprian@bookbub.com>
2015-05-15 13:12:06 -0400

    Add support for scope in associations.

--- lib/active_admin_associations/association_config.rb
+++ lib/active_admin_associations/association_config.rb
@@ -1,26 +1,28 @@
 module ActiveAdminAssociations
   class AssociationConfig
     include Enumerable
-    
+
     class Association
       attr_reader :name
       attr_reader :fields
+      attr_reader :scope
 
-      def initialize(name, fields = [], &block)
+      def initialize(name, fields = [], scope = nil, &block)
         @name = name.to_sym
         @fields = fields
+        @scope = scope
         instance_exec(&block) if block_given?
       end
-      
+
       def field(name)
         @fields << name
       end
-      
+
       def fields(*names)
         @fields += names
       end
     end
-    
+
     attr_reader :association_configs
     delegate :each, :to => :association_configs
 
@@ -28,19 +30,19 @@ module ActiveAdminAssociations
       @association_configs = []
       instance_exec(&block)
     end
-    
+
     def blank?
       @association_configs.blank?
     end
-    
+
     def [](association_name)
       @association_configs.detect {|a| a.name == association_name.to_sym }
     end
-    
-    def association(name, fields = [], &block)
-      @association_configs << Association.new(name, fields, &block)
+
+    def association(name, fields = [], scope = nil, &block)
+      @association_configs << Association.new(name, fields, scope, &block)
     end
-    
+
     def associations(*names)
       names.each do |name|
         @association_configs << Association.new(name)
