115d722883f892a43417707d6b29d9ef42716da9
Dan Thiffault <dthiffault@gmail.com>
2016-07-29 09:36:06 -0400

    Escape values to be displayed in token labels
    If the value of a jquery.token source contained either { or } the
    highlight regex would fail with an exception as documented here
     https://github.com/loopj/jquery-tokeninput/issues/785
    
    Since the upstream project seems to be abandoned, making the fix in our
    vendored version of the library. Also updating to the most recent verion
    of the active admin 1.0.x release candidate.
    
    It seems like the specs in this project have been broken for a while,
    including the updated dummy db schema as a step towards getting those
    straightened out.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source "http://rubygems.org"
 
-gem 'activeadmin', '1.0.0.pre', :github => 'activeadmin/activeadmin', :branch => 'master'
+gem 'activeadmin', '1.0.0.pre4', :github => 'activeadmin/activeadmin', :branch => 'master'
 gem 'sass-rails', '~> 4.0.3'
 gem 'bourbon', '~> 3.2.3'
 gem 'launchy'
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -9,57 +9,57 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120504222247) do
+ActiveRecord::Schema.define(version: 20120504222247) do
 
-  create_table "admin_users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+  create_table "admin_users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
-  add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
+  add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
+  add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
 
-  create_table "posts", :force => true do |t|
+  create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "body"
     t.integer  "creator_id"
     t.datetime "published_at"
     t.boolean  "featured"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "taggings", :force => true do |t|
-    t.integer  "tag_id",        :null => false
-    t.string   "taggable_type", :null => false
-    t.integer  "taggable_id",   :null => false
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+  create_table "taggings", force: true do |t|
+    t.integer  "tag_id",        null: false
+    t.string   "taggable_type", null: false
+    t.integer  "taggable_id",   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "tags", :force => true do |t|
+  create_table "tags", force: true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "users", :force => true do |t|
+  create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
